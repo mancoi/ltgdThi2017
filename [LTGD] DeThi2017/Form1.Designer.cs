8346afba7b64d89c710c41cc7e3c30423427961a
mancoi <caoman96@gmail.com>
2017-01-11 20:13:19 +0700

    first to commit, nothing changed

---
+++ [LTGD] DeThi2017/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace _LTGD__DeThi2017
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xXXToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // picBall
+            // 
+            this.picBall.Image = global::_LTGD__DeThi2017.Properties.Resources.ball;
+            this.picBall.Location = new System.Drawing.Point(111, 118);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(118, 118);
+            this.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBall.TabIndex = 0;
+            this.picBall.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 25;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(342, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.xXXToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // xXXToolStripMenuItem
+            // 
+            this.xXXToolStripMenuItem.Name = "xXXToolStripMenuItem";
+            this.xXXToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.xXXToolStripMenuItem.Text = "XXX";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(342, 305);
+            this.Controls.Add(this.picBall);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xXXToolStripMenuItem;
+    }
+}
+
---
+++ [LTGD] DeThi2017/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace _LTGD__DeThi2017
+{
+    public partial class Form1 : Form
+    {
+        int dx = 8, dy = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            string nhanS = "Nhấn S để bắt đầu/dừng lại";
+            Font f = new Font("Arial", 40, FontStyle.Regular);
+            LinearGradientBrush lnBr = new LinearGradientBrush(new Rectangle(10, 10, 10, 10), Color.Red, Color.Gray, LinearGradientMode.ForwardDiagonal);
+            StringFormat format = new StringFormat();
+            format.LineAlignment = StringAlignment.Near;
+            format.Alignment = StringAlignment.Center;
+
+            Rectangle rec = new Rectangle(0, 40, ClientRectangle.Width, ClientRectangle.Height);
+
+            e.Graphics.DrawString(nhanS, f, lnBr, rec, format);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            picBall.Left = ClientRectangle.Width / 2 - 59;
+            picBall.Top = ClientRectangle.Height / 2 -59;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.S:
+                    timer1.Enabled = !timer1.Enabled;
+                    break;
+                case Keys.Up:
+                    if (dy == 0)
+                        dy = 8;
+                    if (dy > 0)
+                        dy = -dy;
+                    dx = 0;
+                    break;
+                case Keys.Down:
+                    if (dy == 0)
+                        dy = 8;
+                    if (dy < 0)
+                        dy = -dy;
+                    dx = 0;
+                    break;
+                case Keys.Left:
+                    if (dx == 0)
+                        dx = 8;
+                    if (dx > 0)
+                        dx = -dx;
+                    dy = 0;
+                    break;
+                case Keys.Right:
+                    if (dx == 0)
+                        dx = 8;
+                    if (dx < 0)
+                        dx = -dx;
+                    dy = 0;
+                    break;
+                case Keys.PageUp:
+                    if (dx > 0)
+                        dx++;
+                    else if (dx < 0)
+                        dx--;
+                    else if (dy > 0)
+                        dy++;
+                    else if (dy < 0)
+                        dy--;
+                    break;
+                case Keys.PageDown:
+                    if (dx > 0)
+                        dx--;
+                    else if (dx < 0)
+                        dx++;
+                    else if (dy > 0)
+                        dy--;
+                    else if (dy < 0)
+                        dy++;
+                    break;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (picBall.Left <= 0)
+                picBall.Left = ClientRectangle.Width - 118;
+            if (picBall.Left >= ClientRectangle.Width)
+                picBall.Left = 0;
+            if (picBall.Top <= 0)
+                picBall.Top = ClientRectangle.Height - 118;
+            if (picBall.Top >= ClientRectangle.Height)
+                picBall.Top = 0;
+            picBall.Left += dx;
+            picBall.Top += dy;
+        }
+    }
+}
